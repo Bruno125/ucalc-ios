0308c0e794c3130f2a833a8fcb2e8020b5e5afee
Bruno Aybar <miguel_bruno@hotmail.com>
2016-07-29 17:23:54 -0500

    Initial commit

---
+++ uCalc/uCalc/SecondViewController.swift
@@ -0,0 +1,25 @@
+//
+//  SecondViewController.swift
+//  uCalc
+//
+//  Created by Bruno Aybar on 7/29/16.
+//  Copyright © 2016 Bruno Aybar. All rights reserved.
+//
+
+import UIKit
+
+class SecondViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
